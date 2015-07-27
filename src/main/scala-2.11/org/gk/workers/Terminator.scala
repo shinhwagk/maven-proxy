2d4794fa11851cbd9684b08217f07f49461df712
shinhwakd <191631513@qq.com>
2015-07-27 14:43:18 +0800

    0.1.8 基本功能完成

--- src/main/scala-2.11/org/gk/workers/Terminator.scala
+++ src/main/scala-2.11/org/gk/workers/Terminator.scala
@@ -9,25 +9,11 @@ import java.net.Socket;
 /**
  * Created by gk on 15/7/26.
  */
-class Terminator extends Actor{
+class Terminator extends Actor with akka.actor.ActorLogging{
   override def receive: Receive = {
-    case parameter:(Int,Socket) => {
-      closeCode(parameter)
+    case socket:Socket => {
+      socket.close()
+      log.debug("连接关闭...")
     }
-  }
-
-  def closeCode(par:(Int,Socket)) = {
-    par._1 match {
-      case 204 => close_204(par._2);println("关闭无效链接")
-      case 200 => par._2.close()
-    }
-  }
-  def close_204(socket:Socket) = {
-    val bos = new BufferedOutputStream(socket.getOutputStream);
-    val sb = new StringBuilder();
-    sb.append("HTTP/1.1 204 OK\n");
-    sb.append("\n");
-    bos.write(sb.toString().getBytes);
-    bos.flush();
   }
 }
